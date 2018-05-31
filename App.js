734bd9032d5afbf3a2c4d23cc684989a4cf4265f
8p4hxc2 <8p4hxc2@gmail.com>
2018-05-31 16:04:04 +0200

    add main app file

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+StatusBar.setHidden(true);
+
+export default () => (
+    <View>
+        <Text>Test</Text>
+    </View>
+);
