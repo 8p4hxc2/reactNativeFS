aff647fc49058279135cc87f85e33dbec25b1147
8p4hxc2 <8p4hxc2@gmail.com>
2018-06-02 13:55:45 +0200

    Add rotation animation on a Animated.View and toggle it with a button

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { connect, Provider } from 'react-redux';
 import createSagaMiddleware, { delay } from 'redux-saga';
 import { call, put, takeEvery, takeLatest, all } from 'redux-saga/effects'
 import React, { Component } from 'react';
-import { View, StatusBar, Button } from 'react-native';
+import { View, StatusBar, Button, TouchableOpacity, Text, Animated, Image, Easing, Icon } from 'react-native';
 
 StatusBar.setHidden(true);
 
@@ -93,15 +93,75 @@ store.dispatch = logger(store);
 // run saga
 sagaMw.run(aSaga);
 
+// styles
+const styles = {
+    container: {
+        width: '50px',
+        height: '300px'
+    },
+    circle: {
+    }
+}
 
 // entry point component
 class App extends Component {
+    constructor() {
+        super();
+        this.spinValue = new Animated.Value(0);
+    }
+
+    componentDidMount() {
+        this.spin();
+    }
+
+    spin() {
+        this.spinValue.setValue(0);
+        Animated.timing(
+            this.spinValue,
+            {
+                toValue: 1,
+                duration: 4000,
+                easing: Easing.linear
+            }
+        ).start((o) => { if (o.finished) { this.spin(); } })
+    }
+
     render() {
         const { a, dispatch } = this.props;
+        const spin = this.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        });
 
         return (
             <View>
+                <Animated.View
+                    style={{
+                        width: 227,
+                        height: 200,
+                        backgroundColor: 'green',
+                        transform: [{ rotate: spin }]
+                    }}
+                    source={{ uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png' }}
+                />
+
+                <TouchableOpacity onPress={() => {
+                    this.stopped = !this.stopped;
+
+                    if (this.stopped) {
+                        Animated.timing(
+                            this.spinValue
+                        ).stop();
+                    }
+                    else {
+                        this.spin();
+                    }
+                }} style={{ backgroundColor: 'red', height: 100, marginTop: 10 }}>
+                    <Text>MyA button</Text>
+                </TouchableOpacity>
                 <Button
+                    style={styles}
+                    rounded
                     onPress={() => { dispatch(randomizeItem()); }}
                     title={'' + a.number}>
                 </Button>
@@ -114,6 +174,7 @@ class App extends Component {
         );
     };
 };
+
 
 // use the connect helper from redux that will enable the update of App when the redux store is updated
 // fix some issue with double render when doing the subscribe on the store ourself
