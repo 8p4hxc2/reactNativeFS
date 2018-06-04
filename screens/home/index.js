db310914d0b3b0040553781b1758618429682161
8p4hxc2 <8p4hxc2@gmail.com>
2018-06-04 21:21:46 +0200

    continue integrating home disney app

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import { Text, View, Image, ScrollView, FlatList, Linking, TouchableWithoutFeedback } from 'react-native';
+import Hr from '../../components/hr';
 import { connect } from 'react-redux';
 import randomizeItem from '../../actions/splashscreen';
 
 const styles = {
     h1: {
         fontWeight: 'bold',
-        fontSize: 30
+        fontSize: 18
     },
     h2: {
         fontWeight: 'bold',
-        fontSize: 40
+        fontSize: 20,
+        textAlign: 'center',
+        marginLeft: 15,
+        marginRight: 15
     },
     infosItem: {
-        width: 150,
-        height: 75,
+        flex: 1,
         borderRightWidth: 1,
         borderRightColor: 'red'
     }
 }
-
-const But = (item) => {
-    //console.log(item);
-    return (<View><Image />
-        <Text>{item.key}</Text></View>);
-};
 
 class Home extends React.Component {
     render() {
@@ -32,21 +29,40 @@ class Home extends React.Component {
         const { h1, h2, infosItem } = styles;
 
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <ScrollView>
-                    <View style={{ alignItems: 'center', flex: 1 }}>
+            <View style={{ paddingTop: 30, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <ScrollView style={{ flex: 1 }}>
+                    <View style={{ alignItems: 'center', flex: 1, marginTop: 30, marginBottom: 30 }}>
                         <Text style={h1}>Bienvenue !</Text>
                         <Text>à Disneyland Paris</Text>
                     </View>
-                    <Text style={h2} numberOfLines={2} onPress={() => { Linking.openURL('https://google.com'); }}>Information et accès aux Parcs</Text>
+                    <Hr title='Information et accès aux Parcs' width={200} />
                     <FlatList horizontal={true}
+                        style={{ flex: 1, flexDirection: 'row' }}
                         showsHorizontalScrollIndicator={false}
-                        data={[{ key: 'Title 1' }, { key: 'Title 2' }, { key: 'Title 3' }, { key: 'Title 4' }, { key: 'Title 5' }]}
-                        renderItem={({ item }) => <TouchableWithoutFeedback style={infosItem}>
-                            <But item={item} />
-                        </TouchableWithoutFeedback>}
+                        data={[{ key: '1', title: 'Horaire des spectacles du jour' }, { key: '2', title: 'Horaires des parcs' }, { key: '3', title: 'Acheter des billets' }, { key: '4', title: 'Appelez pour acheter vos billets' }, { key: '5', title: 'Réserver une table' }]}
+                        renderItem={({ item }) => {
+                            return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', height: 100, width: 150, ...item.key !== '5' ? { borderRightWidth: 1 } : {} }}>
+                                <Text numberOfLines={1} style={{ textAlign: 'center' }}>{item.title}</Text></View>)
+                        }
+                        }
                     />
-                    <Text></Text>
+                    <Hr title='Lumière sur' />
+                    <View style={{ height: 400, flex: 1 }}>
+                        <View style={{ borderWidth: 1, margin: 25, flex: 1, borderRadius: 5 }}>
+                            <Image resizeMode={'cover'}
+                                style={{ width: '100%', height: 200 }}
+                                source={{ uri: 'http://www.univers-series.com/wp-content/uploads/2017/02/Walt_disney_pictures-750x400.jpg' }} />
+                            <Text  onPress={()=>{this.props.navigation.navigate('About')}} style={{
+                                fontWeight: 'bold',
+                                fontSize: 24
+                            }}>Fêtez notre 25e Anniversaire avec des étoiles plein les yeux !</Text>
+                            <Text>En savoir plus ></Text>
+                        </View>
+                    </View>
+                    <View style={{ backgroundColor: 'lightgray', height: 50, flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ flex: 1, textAlign: 'center', color: 'blue' }}>A propos & Réglement</Text>
+                        <Text style={{ flex: 1, textAlign: 'center', color: 'blue' }}>Mentions légales</Text>
+                    </View>
                 </ScrollView>
             </View >
         );
